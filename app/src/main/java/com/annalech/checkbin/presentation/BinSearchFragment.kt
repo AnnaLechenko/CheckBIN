a2012730a6a4bc74e9012164829fce44537a5a21
Anna <anya.lechenko@mail.ru>
2025-07-06 14:38:52 +0300

    add  view

--- app/src/main/java/com/annalech/checkbin/presentation/BinSearchFragment.kt
+++ app/src/main/java/com/annalech/checkbin/presentation/BinSearchFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.annalech.checkbin.R
@@ -17,7 +19,7 @@ class BinSearchFragment : Fragment(R.layout.bin_search_fragment) {
         get() = _binding ?: throw Exception("No BinSearchFragmentBinding")
 
 
-    val viewModel by lazy {ViewModelProvider(this)[BinViewModel::class.java]}
+    val viewModel by lazy { ViewModelProvider(this)[BinViewModel::class.java] }
 
 
     override fun onCreateView(
@@ -31,8 +33,57 @@ class BinSearchFragment : Fragment(R.layout.bin_search_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getBinInfo("45717360")
-        Log.d("API_LOG","сделан запрос во вью модель во фрагменте")
+
+
+        binding.btnSearch.setOnClickListener {
+            val inputText = binding.etBinInput.text
+            if (inputText.isNotEmpty() && inputText.isDigitsOnly() && inputText.length in 6..8) {
+                viewModel.getBinInfo(inputText.toString()) //"45717360" из примера апи
+                Log.d("API_LOG", "сделан запрос во вью модель во фрагменте")
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Данные введены не верно",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
+
+        viewModel.binInfo.observe(viewLifecycleOwner) {
+            info ->
+
+            binding.tvErrorMessage.visibility = View.GONE
+            binding.resultScrollView.visibility = View.VISIBLE
+
+            val statusPrepaid = if(info.prepaid == true){
+                "Да"
+            }else{
+                "Информация отсутствует"
+            }
+
+            binding.tvScheme.text = "Тип карты: ${info.scheme ?: "Информация отсутствует"}"
+            binding.tvBrand.text =  "Бренд: ${info.brand ?: "Информация отсутствует"}"
+            binding.tvType.text = "Тип: ${info.type ?: "Информация отсутствует"}"
+            binding.tvPrepaid.text = "Предоплаченная: $statusPrepaid"
+            binding.tvCountry.text = "Страна: ${info.country?.name ?: "Информация отсутствует"}"
+
+            binding.tvBankName.text = "Банк: ${info.bank?.name ?: "Информация отсутствует"}"
+            binding.tvBankCity.text = "Город банка: ${info.bank?.city ?: "Информация отсутствует"}"
+            binding.tvBankUrl.text =  "Сайт: ${info.bank?.url ?: "Информация отсутствует"}"
+            binding.tvBankPhone.text = "Телефон: ${info.bank?.phone ?: "Информация отсутствует"}"
+
+        }
+
+        viewModel.isError.observe(viewLifecycleOwner){isError ->
+            if (isError){
+                binding.resultScrollView.visibility = View.GONE
+                binding.tvErrorMessage.visibility = View.VISIBLE
+            }
+
+        }
+
     }
 
     override fun onDestroyView() {
@@ -43,7 +94,7 @@ class BinSearchFragment : Fragment(R.layout.bin_search_fragment) {
     companion object {
 
         fun newInstance(): BinSearchFragment {
-            Log.d("API_LOG","создан фрагмент бин")
+            Log.d("API_LOG", "создан фрагмент бин")
             return BinSearchFragment()
 
         }
--- app/src/main/java/com/annalech/checkbin/presentation/BinViewModel.kt
+++ app/src/main/java/com/annalech/checkbin/presentation/BinViewModel.kt
@@ -1,8 +1,11 @@
 package com.annalech.checkbin.presentation
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.annalech.checkbin.data.network.ApiFactory
+import com.annalech.checkbin.data.network.model.BinInfo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,23 +14,44 @@ import kotlinx.coroutines.withContext
 class BinViewModel : ViewModel() {
 
 
+    private val _isError = MutableLiveData<Boolean>()
+    val isError: LiveData<Boolean>
+        get() = _isError
+
+
+    private val _binInfo = MutableLiveData<BinInfo>()
+    val binInfo: LiveData<BinInfo>
+        get() = _binInfo
+
+
     fun getBinInfo(bin: String) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 Log.d("API_LOG", "старт запроса во вью модель")
 
                 // Выполнение запроса к API
-                val info = ApiFactory.apiService.getBinInfo(bin)
-                withContext(Dispatchers.Main) {
-                    Log.d("API_LOG", "сделан запрос и дальше обработка ответа")
+                val response = ApiFactory.apiService.getBinInfo(bin)
 
-                    // Обработка успешного ответа
-                    if (info.isSuccessful) {
-                        Log.d("API_LOG", "Response: ${info.body()}")
-                    } else {
-                        Log.d("API_LOG", "Response code: ${info.code()}")
+                Log.d("API_LOG", "сделан запрос и дальше обработка ответа")
+
+                // Обработка успешного ответа
+                if (response.isSuccessful) {
+                    val info = response.body()
+                    info?.let { it ->
+                        _binInfo.postValue(it)
+                        _isError.postValue(false)
+                        Log.d("API_LOG", "Response: ${info}")
+                    } ?: run {
+                        _isError.postValue(true)
+                        Log.d("API_LOG", "Response null: ${info}")
                     }
+
+
+                } else {
+                    _isError.postValue(true)
+                    Log.d("API_LOG", "Response code: ${response.code()}")
                 }
+
 
             } catch (e: Exception) {
                 // Логирование ошибок
