48c348f063fee1454971910d1021dd417b2c5767
Anna <anya.lechenko@mail.ru>
2025-07-06 09:26:41 +0300

    add dependency

---
+++ app/src/main/java/com/annalech/checkbin/data/database/SearchResponseDBModel.kt
@@ -0,0 +1,2 @@
+package com.annalech.checkbin.data.database
+
---
+++ app/src/main/java/com/annalech/checkbin/data/network/ApiFactory.kt
@@ -0,0 +1,10 @@
+package com.annalech.checkbin.data.network
+
+import retrofit2.Retrofit
+
+object ApiFactory {
+
+    val retrofit = Retrofit.Builder()
+
+
+}
